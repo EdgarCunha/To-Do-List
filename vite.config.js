c9ebdd88eb72e565b4e897a592f9b6fdfc10f768
Edgar Cunha <renatolfsantos@gmail.com>
2024-03-07 19:19:11 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://EdgarCunha.github.io/",
+  base: "https://edgarcunha.github.io/toDoList/",
 });
